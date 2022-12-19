90150b04e8dce68c3c74524f5835d92861bf40f1
Yan Ruzika di Laptop Lenovo Garuda Linux <yanruziqa@gmail.com>
2022-12-20 06:36:25 +0700

    quick fix

--- config/global.php
+++ config/global.php
@@ -1,4 +1,4 @@
 <?php
-    $f3->set('base', 'http://localhost/ruang-seminar-landing-page/');
+    $f3->set('base', 'https://develop.ruangseminar.id/');
     $f3->set('baseOnlineResources', 'https://ruangseminar.site/');
 ?>
